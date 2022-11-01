e6552cae5c05b6885ba2360f7d1c64dfdafbc95a
ryzhikovpavel <ryzhikovpavel@gmail.com>
2022-11-01 21:19:52 +0400

    refactoring

--- Runtime/Debugging/LogType.cs
+++ Runtime/Debugging/LogType.cs
@@ -6,13 +6,14 @@ namespace GameKit
     public enum LogType
     {
         Disable = 0,
+        
+        All = Debug | Info | Warning | Error,
+        Normal = Info | Warning | Error,
+        Important = Warning | Error,
+        
         Debug = 1,
         Info = 2,
         Warning = 4,
-        Error = 8,
-        
-        Normal = Info | Warning | Error,
-        Important = Warning | Error,
-        All = Debug | Info | Warning | Error 
+        Error = 8
     }
 }
