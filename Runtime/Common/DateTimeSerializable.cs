7fe51238322b0f7884f85e2af2f7dfc1eb3dcff1
ryzhikovpavel <ryzhikovpavel@gmail.com>
2023-01-12 21:14:45 +0400

    Fix datetime

--- Runtime/Common/DateTimeSerializable.cs
+++ Runtime/Common/DateTimeSerializable.cs
@@ -1,25 +1,63 @@
 ﻿using System;
+using System.Globalization;
+using JetBrains.Annotations;
+using UnityEngine;
 
 namespace GameKit
 {
+    [PublicAPI]
     [Serializable]
     public struct DateTimeSerializable
     {
-        public long value;
-        public static implicit operator DateTime(DateTimeSerializable jdt)
+        [SerializeField]
+        private double unixTime;
+        private DateTime? _time;
+
+        private DateTime GetTime()
         {
-            return DateTime.FromFileTime(jdt.value);
+            if (_time.HasValue) return _time.Value;
+            _time = unixTime.FromUnixTimestamp();
+            return _time.Value;
+        }
+
+        public static implicit operator DateTime(DateTimeSerializable other)
+        {
+            return other.GetTime();
         }
         public static implicit operator DateTimeSerializable(DateTime dt)
         {
-            DateTimeSerializable jdt = new DateTimeSerializable();
-            jdt.value = dt.ToFileTime();
-            return jdt;
+            return new DateTimeSerializable
+            {
+                unixTime = dt.ToUnixTimestamp()
+            };
         }
 
         public override string ToString()
         {
-            return DateTime.FromFileTime(value).ToString();
+            return GetTime().ToString(CultureInfo.InvariantCulture);
         }
+
+        public System.DateTime Date => GetTime().Date;
+        public int Day => GetTime().Day;
+        public System.DayOfWeek DayOfWeek => GetTime().DayOfWeek;
+        public int DayOfYear => GetTime().DayOfYear;
+        public int Hour => GetTime().Hour;
+        public System.DateTimeKind Kind => GetTime().Kind;
+        public int Millisecond => GetTime().Millisecond;
+        public int Minute => GetTime().Minute;
+        public int Month => GetTime().Month;
+        public int Second => GetTime().Second;
+        public long Ticks => GetTime().Ticks;
+        public System.TimeSpan TimeOfDay => GetTime().TimeOfDay;
+        public int Year => GetTime().Year;
+        public System.DateTime Add(System.TimeSpan value) => GetTime().Add(value);
+        public System.DateTime AddDays(double value) => GetTime().AddDays(value);
+        public System.DateTime AddHours(double value) => GetTime().AddHours(value);
+        public System.DateTime AddMilliseconds(double value) => GetTime().AddMilliseconds(value);
+        public System.DateTime AddMinutes(double value) => GetTime().AddMinutes(value);
+        public System.DateTime AddMonths(int months) => GetTime().AddMonths(months);
+        public System.DateTime AddSeconds(double value) => GetTime().AddSeconds(value);
+        public System.DateTime AddTicks(long value) => GetTime().AddTicks(value);
+        public System.DateTime AddYears(int value) => GetTime().AddYears(value);
     }
 }
--- Runtime/Extensions/DateTimeExtensions.cs
+++ Runtime/Extensions/DateTimeExtensions.cs
@@ -5,14 +5,14 @@ namespace GameKit
 {
     public static class DateTimeExtensions
     {
-        public static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
         public static double ToUnixTimestamp(this DateTime dateTime)
         {
             return (dateTime.Subtract(UnixEpoch)).TotalSeconds;
         }
         
-        public static DateTime FromUnixTimestamp(double timeStamp)
+        public static DateTime FromUnixTimestamp(this double timeStamp)
         {
             return UnixEpoch.AddSeconds(timeStamp);
         }
